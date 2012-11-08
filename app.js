f1fb6f4b128dc67caa1702172645c7e71e1b48f6
Romain Ruetschi <romain.ruetschi@gmail.com>
2012-11-09 00:09:30 +0100

    Add people behing HackEPFL to the website.

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 
-var fb   = require( './lib/fb' ),
+var http = require( 'http' ),
+    url = require( 'url' ),
+    request = require( 'request' ),
+    FB = require( './lib/fb' ),
     fetchData = require( './lib/data' ).fetchAll,
     express = require( 'express' ),
     app = module.exports = express(),
-    data = {};
+    data = {},
+    people = require( './lib/people' );
 
 function refreshData()
 {
-    fb.connect( function( FB )
+    FB.__connect( function( FB )
     {
+        accessToken = FB.getAccessToken();
+
         fetchData( FB, function( err, results )
         {
             data.albums = results.albums;
@@ -44,6 +50,14 @@ app.get( '/events', function( req, res )
     } );
 } );
 
+app.get( '/people', function( req, res )
+{
+    res.render( 'people', {
+        title: 'People',
+        people: people
+    } );
+} );
+
 app.get( '/gallery', function( req, res )
 {
     res.render( 'gallery', {
@@ -69,6 +83,20 @@ app.get( '/album/:id', function( req, res )
         title: album.name,
         album: album
     } );
+} );
+
+// I'm lazy. Will eventually^Wdefinitely have to switch to proper resources linking.
+// Eg. https://graph.facebook.com/43543534254353/picture?type=large => http://photos-d.ak.fbcdn.net/[...]
+app.get( '/fb/*', function( req, res )
+{
+    var uri = url.parse( req.url.replace( /^\/fb/, '' ), true );
+    uri.protocol = 'https:';
+    uri.host = 'graph.facebook.com';
+    uri.query.access_token = FB.getAccessToken();
+    uri.search = undefined;
+    uri = url.format( uri );
+
+    request( uri ).pipe( res );
 } );
 
 app.use( express.static( __dirname + '/public' ) );
